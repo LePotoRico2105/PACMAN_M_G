0b6248e946580a80095b146b6cca5b4eda4aa3ce
LePotoRico2105 <matthieu.david2105@gmail.com>
2020-03-30 08:18:57 +0200

    class Mur

---
+++ PACMAN_DAVID_GILLET/src/modele/Mur.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.util.Random;
+
+/**
+ *
+ * @author david
+ */
+public class Mur {
+    private String type;
+    private Jeu jeu;
+    private int rotationType;
+      
+    public Mur(Jeu _jeu, String p_type, int p_rotationType) {
+        jeu = _jeu;
+        type = p_type;
+        rotationType = p_rotationType;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    public int getRotationType(){
+        return rotationType;
+    }
+}
