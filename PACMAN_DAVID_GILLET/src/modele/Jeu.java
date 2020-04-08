44efaa47702f579b51d04344198623a5fca06394
Guillaume Gillet <gillet.guillaume@etu.univ-lyon1.fr>
2020-04-08 19:34:54 +0200

    Mise au propre du code

--- PACMAN_DAVID_GILLET/src/modele/Jeu.java
+++ PACMAN_DAVID_GILLET/src/modele/Jeu.java
@@ -89,6 +89,7 @@ public class Jeu extends Observable implements Runnable {
         grilleEntites[9][15] = pm;
         map.put(pm, new Point(9, 15));
         
+        
         // Initialisation des fantomes
         bleu = new Fantome(this, "bleu");
         rose = new Fantome(this, "rose");
